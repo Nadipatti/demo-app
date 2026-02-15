23c596dbe13e920fb03c57308fa54489d2d2ed3d
Kavitha Nadipati <nadipattikavitha@gmail.com>
2026-02-15 08:14:50 +0530

    Initial commit - Spring Boot Docker app

---
+++ src/main/java/com/devops/demo/HelloController.java
@@ -0,0 +1,18 @@
+package com.devops.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello DevOps!";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "Application is running!";
+    }
+}
